c8554252f4be4a1d8d74c31d2320ac84dc89968c
SanaaElattari <sanaaelattari2006@gmail.com>
2025-09-01 19:05:53 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -93,7 +93,7 @@ app.get("*", (req, res) => {
 
 // Start server
 loadData().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 });
