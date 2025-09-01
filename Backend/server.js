37c2c4a5931a84ee36dc7eab311a45892fb2a7ac
SanaaElattari <sanaaelattari2006@gmail.com>
2025-09-01 10:35:53 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 let offenseData = [];
 let defenseData = [];
