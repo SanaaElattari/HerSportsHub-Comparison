5abff1bbdc74afac3ab48d72ea8dbc55a903764f
SanaaElattari <sanaaelattari2006@gmail.com>
2025-09-01 18:41:45 -0400

    Update Comparison.jsx

--- womens-soccer-frontend/src/components/Comparison.jsx
+++ womens-soccer-frontend/src/components/Comparison.jsx
@@ -37,12 +37,12 @@ const Comparison = () => {
   useEffect(() => {
     const fetchAllPlayers = async () => {
       try {
-        const teamsRes = await fetch("/teams");
+        const teamsRes = await fetch("https://node-service-production.up.railway.app/teams");
         const teams = await teamsRes.json();
 
         const allPlayersData = [];
         for (const team of teams) {
-          const res = await fetch(`/players?team=${encodeURIComponent(team)}`);
+          const res = await fetch(`https://node-service-production.up.railway.app/players?team=${encodeURIComponent(team)}`);
           const teamPlayers = await res.json();
           allPlayersData.push(...teamPlayers);
         }
